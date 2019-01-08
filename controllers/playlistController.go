18f9b9b11d8b523a360a8d3c819687d93de38b60
PierreBougon <pierre.bougon@epitech.eu>
2019-01-08 02:28:45 +0100

    Playlist model + controller

---
+++ controllers/playlistController.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"Bym-BackEnd/models"
+	u "Bym-BackEnd/utils"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+var CreatePlaylist = func(w http.ResponseWriter, r *http.Request) {
+
+	user := r.Context().Value("user").(uint)
+	var playlist = &models.Playlist{}
+	err := json.NewDecoder(r.Body).Decode(playlist) //decode the request body into struct and failed if any error occur
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := playlist.Create(user)
+	u.Respond(w, resp)
+}
+
+var GetPlaylists = func(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user").(uint)
+
+	data := models.GetPlaylists(uint(user))
+	resp := u.Message(true, "success")
+	resp["playlists"] = data
+	u.Respond(w, resp)
+}
+
+var GetPlaylist = func(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		//The passed path parameter is not an integer
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	data := models.GetPlaylist(uint(id))
+	resp := u.Message(true, "success")
+	resp["playlist"] = data
+	u.Respond(w, resp)
+}
---
+++ models/playlists.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	u "Bym-BackEnd/utils"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+type Playlist struct {
+	gorm.Model
+	Name   string `json:"name"`
+	UserId uint   `json:"user_id"`
+	Songs  []Song
+}
+
+func (playlist *Playlist) Validate() (map[string]interface{}, bool) {
+	if len(playlist.Name) < 3 {
+		return u.Message(false, "Playlist name must be at least 3 characters long"), false
+	}
+	if playlist.UserId <= 0 {
+		return u.Message(false, "Invalid user"), false
+	}
+	return u.Message(false, "Requirement passed"), true
+}
+
+func (playlist *Playlist) Create(user uint) map[string]interface{} {
+
+	playlist.UserId = user
+	if resp, ok := playlist.Validate(); !ok {
+		return resp
+	}
+
+	GetDB().Create(playlist)
+
+	if playlist.ID <= 0 {
+		return u.Message(false, "Failed to create playlist, connection error.")
+	}
+
+	response := u.Message(true, "Playlist has been created")
+	response["playlist"] = playlist
+	return response
+}
+
+func GetPlaylist(u uint) *Playlist {
+	playlist := &Playlist{}
+	GetDB().Table("playlists").Where("id = ?", u).First(playlist)
+	if playlist.Name == "" {
+		return nil
+	}
+	return playlist
+}
+
+func GetPlaylists(user uint) []*Playlist {
+
+	playlists := make([]*Playlist, 0)
+	err := GetDB().Table("playlists").Where("user_id = ?", user).Find(&playlists).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return playlists
+}
