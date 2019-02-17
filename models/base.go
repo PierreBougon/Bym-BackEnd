a2f6e77b2dee0e82d7ef23fa8931cb4c604d1aec
guilbo_m <mathis.guilbon@epitech.eu>
2019-02-17 14:37:49 +0100

    use DATABASE_URL by default to connect to the db

--- models/base.go
+++ models/base.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -13,20 +14,31 @@ var db *gorm.DB //database
 
 func init() {
 
-	e := godotenv.Load() //Load .env file
-	if e != nil {
-		fmt.Print(e)
+	dbUrl := os.Getenv("DATABASE_URL")
+	dbLang := "postgres"
+	dbUri := ""
+
+	if  len(dbUrl) > 3 {
+		parts := strings.Split(dbUrl, "://")
+		dbLang = parts[0]
+		dbUri = parts[1]
+
+	} else {
+		e := godotenv.Load() //Load .env file
+		if e != nil {
+			fmt.Print(e)
+		}
+
+		username := os.Getenv("db_user")
+		password := os.Getenv("db_pass")
+		dbName := os.Getenv("db_name")
+		dbHost := os.Getenv("db_host")
+
+		dbUri = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
+		fmt.Println(dbUri)
 	}
 
-	username := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
-
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
-	fmt.Println(dbUri)
-
-	conn, err := gorm.Open("postgres", dbUri)
+	conn, err := gorm.Open(dbLang, dbUri)
 	if err != nil {
 		fmt.Print(err)
 	}
