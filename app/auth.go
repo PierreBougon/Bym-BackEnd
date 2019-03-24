e3101aeb6a0d4c9a8206d6a0cb156b46ef7bce87
guilbo_m <mathis.guilbon@epitech.eu>
2019-03-24 21:28:29 +0800

    test

--- app/auth.go
+++ app/auth.go
@@ -15,6 +15,14 @@ import (
 var JwtAuthentication = func(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var sendErrorJson = func(w http.ResponseWriter, errMsg string, httpStatus int) {
+			response := make(map[string]interface{})
+
+			response = u.Message(false, errMsg)
+			w.WriteHeader(httpStatus)
+			w.Header().Add("Content-Type", "application/json")
+			u.Respond(w, response)
+		}
 
 		notAuth := []string{"/api/user/new", "/api/user/login"} //List of endpoints that doesn't require auth
 		requestPath := r.URL.Path                               //current request path
@@ -28,23 +36,15 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 			}
 		}
 
-		response := make(map[string]interface{})
 		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
-
 		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
-			response = u.Message(false, "Missing auth token")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
+			sendErrorJson(w, "Missing auth token", http.StatusForbidden)
 			return
 		}
 
 		splitted := strings.Split(tokenHeader, " ") //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
 		if len(splitted) != 2 {
-			response = u.Message(false, "Invalid/Malformed auth token")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
+			sendErrorJson(w, "Invalid/Malformed auth token", http.StatusForbidden)
 			return
 		}
 
@@ -56,18 +56,12 @@ var JwtAuthentication = func(next http.Handler) http.Handler {
 		})
 
 		if err != nil { //Malformed token, returns with http code 403 as usual
-			response = u.Message(false, "Malformed authentication token")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
+			sendErrorJson(w, "Malformed authentication token", http.StatusForbidden)
 			return
 		}
 
 		if !token.Valid { //Token is invalid, maybe not signed on this server
-			response = u.Message(false, "Token is not valid.")
-			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type", "application/json")
-			u.Respond(w, response)
+			sendErrorJson(w, "Token is not valid.", http.StatusForbidden)
 			return
 		}
 
---
+++ app/auth_test.go
@@ -0,0 +1,12 @@
+package app_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/PierreBougon/Bym-BackEnd/app"
+)
+
+var _ = Describe("Auth", func() {
+
+})
