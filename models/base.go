c75b80d3a258618ca6cbd56ece0669f28af10f1f
guilbo_m <mathis.guilbon@epitech.eu>
2019-03-30 23:17:57 +0800

    heroku db connexion fixed

--- models/base.go
+++ models/base.go
@@ -2,48 +2,58 @@ package models
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"os"
 	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
 )
 
 var db *gorm.DB //database
 
-func init() {
+func migrate() {
+	db.AutoMigrate(
+		&Account{},
+		&Playlist{},
+		&Song{},
+		)
+}
 
+func getDbInfoFromEnv() (dbLang string, dbUri string) {
 	dbUrl := os.Getenv("DATABASE_URL")
-	dbLang := "postgres"
-	dbUri := ""
-
-	if  len(dbUrl) > 3 {
+	if  len(dbUrl) > 4 { // need a regex check to validate
 		parts := strings.Split(dbUrl, "://")
 		dbLang = parts[0]
 		dbUri = parts[1]
-
 	} else {
 		e := godotenv.Load() //Load .env file
 		if e != nil {
 			fmt.Print(e)
 		}
 
+		dbLang := os.Getenv("db_dialect")
 		username := os.Getenv("db_user")
 		password := os.Getenv("db_pass")
 		dbName := os.Getenv("db_name")
 		dbHost := os.Getenv("db_host")
 
-		dbUri = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password) //Build connection string
-		fmt.Println(dbUri)
+		dbUri = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s sslmode=require", dbHost, username, dbName, password) //Build connection string
 	}
+	fmt.Println(dbLang, dbUri)
+	return
+}
 
-	conn, err := gorm.Open(dbLang, dbUri)
+func init() {
+	// dbLang, dbUri := getDbInfoFromEnv()
+	conn, err := gorm.Open(getDbInfoFromEnv())//(dbLang, dbUri)
 	if err != nil {
 		fmt.Print(err)
+		panic("failed to connect to database")
 	}
 
 	db = conn
+	// migrate()
 	db.Debug().AutoMigrate(&Account{}, &Playlist{}, &Song{}) //Database migration
 }
 
