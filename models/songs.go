7a08803c5a56e5001696863cfa9a0f73891b795c
PierreBougon <pierre.bougon@epitech.eu>
2019-05-04 08:22:04 +0200

    Working on rankings : add a way to get rankings only as a part of songs

--- models/songs.go
+++ models/songs.go
@@ -13,7 +13,19 @@ type Song struct {
 	Name       string `json:"name"`
 	PlaylistId uint   `json:"playlist_id"`
 	ExternalId string `json:"external_id"`
+	VoteUp     int    `json:"vote_up"`
+	VoteDown   int    `json:"vote_down"`
+	Score      int    `json:"score"`
+
 	// We can add image + infos etc
+}
+
+// Not a model used to hold part of the song model
+type Ranking struct {
+	SongId   uint `json:"song_id"`
+	VoteUp   int  `json:"vote_up"`
+	VoteDown int  `json:"vote_down"`
+	Score    int  `json:"score"`
 }
 
 func (song *Song) Validate(user uint) (map[string]interface{}, bool) {
@@ -87,4 +99,69 @@ func (song *Song) DeleteSong(user uint, songId uint) map[string]interface{} {
 	}
 	db.Delete(&retSong)
 	return u.Message(true, "Song successfully deleted")
+}
+
+func GetSongsRanking(playlist uint) []*Ranking {
+
+	songs := make([]*Song, 0)
+	err := GetDB().Table("songs").Where("playlist_id = ?", playlist).Find(&songs).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	size := len(songs)
+	songJson := Song{}
+	ranks := make([]*Ranking, size)
+	for j := 0; j < len(ranks); j++ {
+		ranks[j] = &Ranking{}
+	}
+	for i := 0; i < size; i++ {
+		songJson = *songs[i]
+		ranks[i].SongId = songJson.ID
+		ranks[i].VoteUp = songJson.VoteUp
+		ranks[i].VoteDown = songJson.VoteDown
+		ranks[i].Score = songJson.Score
+	}
+
+	return ranks
+}
+
+func GetSongRankingById(songid uint) *Ranking {
+
+	song := Song{}
+	err := GetDB().Table("songs").Where("id = ?", songid).Find(&song).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	rank := Ranking{}
+	rank.SongId = song.ID
+	rank.VoteUp = song.VoteUp
+	rank.VoteDown = song.VoteDown
+	rank.Score = song.Score
+
+	return &rank
+}
+
+func RefreshSongVotes(songid uint) {
+	//TODO : do not call this method directly create a thread to handle the refresh in another thread with a correct delay to not make 100 refresh/s for now just use it as it is
+	//votes := make([]*Vote, 0)
+	upVotes := 0
+	downVotes := 0
+	err1 := GetDB().Table("votes").Where("song_id = ? AND up_vote = ?", songid, true).Count(&upVotes).Error
+	err2 := GetDB().Table("votes").Where("song_id = ? AND down_vote = ?", songid, true).Count(&downVotes).Error
+	if err1 != nil || err2 != nil {
+		return
+	}
+	song := Song{}
+	err := db.Table("songs").First(&song, songid).Error
+	if err != nil {
+		return
+	}
+	song.VoteUp = upVotes
+	song.VoteDown = downVotes
+	song.Score = upVotes*100 - downVotes*100
+	db.Save(&song)
 }
