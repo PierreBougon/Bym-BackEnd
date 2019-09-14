836534e58674a624840b28891a5dbcdac383446a
guilbo_m <mathis.guilbon@epitech.eu>
2019-09-14 12:39:11 +0200

    adding the user_id in the moesif metadata, sorting by user_id is now possible in analytics

--- moesif/moesif.go
+++ moesif/moesif.go
@@ -1,10 +1,20 @@
 package moesif
 
 import (
+	"fmt"
 	"github.com/moesif/moesifmiddleware-go"
 	"net/http"
 	"os"
 )
+
+func MiddlewareWrapper(h http.Handler) http.Handler {
+	options := fetchMoesifOptions()
+	if options == nil {
+		return h
+	}
+	
+	return moesifmiddleware.MoesifMiddleware(h, options)
+}
 
 func fetchMoesifOptions() map[string]interface{}{
 	appId := os.Getenv("moesif_app_id")
@@ -12,17 +22,13 @@ func fetchMoesifOptions() map[string]interface{}{
 	if appId == "" {
 		return nil
 	}
+
 	return map[string]interface{} {
 		"Application_Id": appId,
 		"Log_Body": true,
 		"Capture_Outoing_Requests": true,
+		"Identify_User": func (r *http.Request, recorder moesifmiddleware.MoesifResponseRecorder) string {
+			return fmt.Sprint(r.Context().Value("user"))
+		},
 	}
-}
-
-func MiddlewareWrapper(h http.Handler) http.Handler {
-	options := fetchMoesifOptions()
-	if options == nil {
-		return h
-	}
-	return moesifmiddleware.MoesifMiddleware(h, options)
 }
