1858c7e1335d409b7f4361713d31926f0a1b2832
PierreBougon <pierre.bougon@epitech.eu>
2019-09-18 19:29:07 +0200

    Add / and /api route with a basic success response to let everyone know that the server is up

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/PierreBougon/Bym-BackEnd/app"
 	"github.com/PierreBougon/Bym-BackEnd/controllers"
 	"github.com/PierreBougon/Bym-BackEnd/moesif"
+	u "github.com/PierreBougon/Bym-BackEnd/utils"
 
 	"fmt"
 	"net/http"
@@ -17,7 +18,8 @@ func main() {
 	router := mux.NewRouter()
 	router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	// Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "443"
 	}
@@ -27,6 +29,10 @@ func main() {
 	// API
 	api := router.PathPrefix("/api").Subrouter()
 	api.Use(moesif.MiddlewareWrapper)
+
+	// Respond a basic success if anyone wants to get from / or /api to let them now the url is correct and server is up
+	router.HandleFunc("", u.RespondBasicSuccess).Methods("GET")
+	api.HandleFunc("", u.RespondBasicSuccess).Methods("GET")
 
 	//		Auth
 	auth := api.PathPrefix("/user").Subrouter()
@@ -49,7 +55,7 @@ func main() {
 	song.HandleFunc("/{id}", controllers.UpdateSong).Methods("PUT")
 	song.HandleFunc("/{id}", controllers.DeleteSong).Methods("DELETE")
 
-	//			Ranking (Fraction of Songs parsed to access it directly)
+	//		Ranking (Fraction of Songs parsed to access it directly)
 	ranking := song.PathPrefix("/ranking").Subrouter()
 	ranking.HandleFunc("", controllers.GetRankings).Methods("GET")
 	ranking.HandleFunc("/{id}", controllers.GetRanking).Methods("GET")
@@ -60,7 +66,8 @@ func main() {
 	vote.HandleFunc("", controllers.GetVote).Methods("GET")
 	//	vote.HandleFunc("/{id}", controllers.DeleteSong).Methods("DELETE")
 
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	//Launch the app, visit localhost:443/api
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		fmt.Print(err)
 	}
