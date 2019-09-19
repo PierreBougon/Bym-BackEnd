fe5538c3732c48047c177f0cc354c4d67d14e8c1
PierreBougon <pierre.bougon@epitech.eu>
2019-09-19 08:45:34 +0200

    Try a fix on moesif to handle a crash from the service

--- moesif/moesif.go
+++ moesif/moesif.go
@@ -12,23 +12,26 @@ func MiddlewareWrapper(h http.Handler) http.Handler {
 	if options == nil {
 		return h
 	}
-	
+
 	return moesifmiddleware.MoesifMiddleware(h, options)
 }
 
-func fetchMoesifOptions() map[string]interface{}{
+func fetchMoesifOptions() map[string]interface{} {
 	appId := os.Getenv("moesif_app_id")
 
 	if appId == "" {
 		return nil
 	}
 
-	return map[string]interface{} {
-		"Application_Id": appId,
-		"Log_Body": true,
+	return map[string]interface{}{
+		"Application_Id":           appId,
+		"Log_Body":                 true,
 		"Capture_Outoing_Requests": true,
-		"Identify_User": func (r *http.Request, recorder moesifmiddleware.MoesifResponseRecorder) string {
-			return fmt.Sprint(r.Context().Value("user"))
+		"Identify_User": func(r *http.Request, recorder moesifmiddleware.MoesifResponseRecorder) string {
+			if user := r.Context().Value("user"); user != nil {
+				return fmt.Sprint(user)
+			}
+			return "unauthenticated"
 		},
 	}
 }
