cf32efa3f59065b91fabd77885084840c47870bb
PierreBougon <pierre.bougon@epitech.eu>
2019-12-01 18:57:13 +0100

    Change websocket_communication to websocket

--- controllers/websocketController.go
+++ controllers/websocketController.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"fmt"
-	"github.com/PierreBougon/Bym-BackEnd/websocket_communication"
+	"github.com/PierreBougon/Bym-BackEnd/websocket"
 	"net/http"
 )
 
@@ -10,7 +10,7 @@ var ConnectWebSocket = func(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Trying to establish a connection")
 	fmt.Println(r.Header.Get("Sec-WebSocket-Protocol")) //Grab the token from the header
 
-	var wsPool *websocket_communication.WSPool
-	wsPool = websocket_communication.GetWSPool()
+	var wsPool *websocket.WSPool
+	wsPool = websocket.GetWSPool()
 	wsPool.CreateWebSocket(w, r)
 }
